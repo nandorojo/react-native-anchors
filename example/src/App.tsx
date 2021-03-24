065a42b245e850f75a19d78f8c3394652084887c
Fernando Rojo <frojo@sas.upenn.edu>
2021-03-24 19:00:44 -0400

    feat: horizontal support

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,27 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import Anchor from '@nandorojo/anchor';
+import { Target, ScrollView, Anchor } from '@nandorojo/anchor';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    Anchor.multiply(3, 7).then(setResult);
-  }, []);
-
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <ScrollView>
+        <View style={styles.box} />
+        <Target name="top">
+          <Anchor target="bottom">
+            <Text>Scroll down</Text>
+          </Anchor>
+        </Target>
+        <View style={styles.box} />
+        <Target name="bottom">
+          <Anchor target="top">
+            <Text>Scroll up</Text>
+          </Anchor>
+        </Target>
+        <View style={styles.box} />
+        <View style={styles.box} />
+      </ScrollView>
     </View>
   );
 }
@@ -20,12 +29,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   box: {
-    width: 60,
-    height: 60,
+    width: 150,
+    height: 300,
     marginVertical: 20,
+    backgroundColor: 'blue',
   },
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,369 @@
-export default {
-  multiply(a: number, b: number) {
-    return Promise.resolve(a * b);
-  },
+import * as React from 'react';
+import {
+  View,
+  Text,
+  ScrollView as NativeScrollView,
+  FlatList as NativeFlatList,
+  FlatListProps,
+  findNodeHandle,
+  Pressable,
+} from 'react-native';
+import type {
+  ComponentProps,
+  MutableRefObject,
+  LegacyRef,
+  ReactNode,
+  RefObject,
+  RefCallback,
+} from 'react';
+
+const { createContext, forwardRef, useContext, useMemo, useRef } = React;
+
+// from react-merge-refs (avoid dependency)
+function mergeRefs<T = any>(
+  refs: Array<MutableRefObject<T> | LegacyRef<T>>
+): RefCallback<T> {
+  return (value) => {
+    refs.forEach((ref) => {
+      if (typeof ref === 'function') {
+        ref(value);
+      } else if (ref != null) {
+        (ref as MutableRefObject<T | null>).current = value;
+      }
+    });
+  };
+}
+
+// type Props<Anchors extends readonly string[]> = {
+//   anchors: Anchors;
+// };
+
+type Unpromisify<T> = T extends Promise<infer R> ? R : T;
+
+/**
+ * The following is taken/edited from `useScrollToTop` from `@react-navigation/native`
+ */
+type ScrollOptions = { y?: number; animated?: boolean };
+
+type ScrollableView =
+  | { scrollToTop(): void }
+  | { scrollTo(options: ScrollOptions): void }
+  | { scrollToOffset(options: { offset?: number; animated?: boolean }): void }
+  | { scrollResponderScrollTo(options: ScrollOptions): void };
+
+type ScrollableWrapper =
+  | { getScrollResponder(): ReactNode }
+  | { getNode(): ScrollableView }
+  | ScrollableView;
+
+function getScrollableNode(ref: RefObject<ScrollableWrapper>) {
+  if (ref.current == null) {
+    return null;
+  }
+
+  if (
+    'scrollTo' in ref.current ||
+    'scrollToOffset' in ref.current ||
+    'scrollResponderScrollTo' in ref.current
+  ) {
+    // This is already a scrollable node.
+    return ref.current;
+  } else if ('getScrollResponder' in ref.current) {
+    // If the view is a wrapper like FlatList, SectionList etc.
+    // We need to use `getScrollResponder` to get access to the scroll responder
+    return ref.current.getScrollResponder();
+  } else if ('getNode' in ref.current) {
+    // When a `ScrollView` is wraped in `Animated.createAnimatedComponent`
+    // we need to use `getNode` to get the ref to the actual scrollview.
+    // Note that `getNode` is deprecated in newer versions of react-native
+    // this is why we check if we already have a scrollable node above.
+    return ref.current.getNode();
+  } else {
+    return ref.current;
+  }
+}
+/**
+ * End of react-navigation code.
+ */
+
+type ScrollToOptions = {
+  animated?: boolean;
+  /**
+   * A number that determines how far from the content you want to scroll.
+   *
+   * Default: `-10`, which means it scrolls to 10 pixels before the content.
+   */
+  offset?: number;
+  /**
+   * If you're using a `ScrollView` or `FlatList` imported from this library, you can ignore this field.
+   */
+  // horizontal?: boolean;
 };
+
+export interface Anchors {}
+
+// @ts-expect-error
+type Anchor = Anchors['anchor'] extends string ? Anchors['anchor'] : string;
+
+// export default function createAnchors() {
+type AnchorsContext = {
+  targetRefs: RefObject<Record<Anchor, View | Text>>;
+  scrollRef: RefObject<ScrollableWrapper>;
+  registerTargetRef: (name: Anchor, ref: View | Text) => void;
+  registerScrollRef: (ref: ScrollableWrapper | null) => void;
+  horizontal: ComponentProps<typeof NativeScrollView>['horizontal'];
+};
+
+const AnchorsContext = createContext<AnchorsContext>({
+  targetRefs: {
+    current: {} as any,
+  },
+  scrollRef: {
+    current: null as any,
+  },
+  registerTargetRef: () => {
+    // no-op
+  },
+  registerScrollRef: () => {
+    // no-op
+  },
+  horizontal: false,
+});
+
+const useAnchorsContext = () => useContext(AnchorsContext);
+
+const useCreateAnchorsContext = ({
+  horizontal,
+}: Pick<AnchorsContext, 'horizontal'>): AnchorsContext => {
+  const targetRefs = useRef<Record<string, View>>({});
+  const scrollRef = useRef<ScrollableWrapper>();
+
+  return useMemo(() => {
+    return {
+      targetRefs,
+      scrollRef: scrollRef as RefObject<ScrollableWrapper>,
+      registerTargetRef: (target, ref) => {
+        targetRefs.current = {
+          ...targetRefs.current,
+          [target]: ref,
+        };
+      },
+      registerScrollRef: (ref) => {
+        if (ref) {
+          scrollRef.current = ref;
+        }
+      },
+      horizontal,
+    };
+  }, [horizontal]);
+};
+
+function useRegisterTarget() {
+  const { registerTargetRef } = useAnchorsContext();
+
+  return useMemo(
+    () => ({
+      register: (name: Anchor) => {
+        return (ref: View) => registerTargetRef(name, ref);
+      },
+    }),
+    [registerTargetRef]
+  );
+}
+
+function useScrollTo() {
+  const { targetRefs, scrollRef, horizontal } = useAnchorsContext();
+
+  return useMemo(
+    () => ({
+      scrollTo: (
+        name: Anchor,
+        { animated = true, offset = -10 }: ScrollToOptions = {}
+      ) => {
+        return new Promise<
+          { success: true } | { success: false; message: string }
+        >((resolve) => {
+          const node =
+            scrollRef.current && findNodeHandle(scrollRef.current as any);
+          if (!node || !scrollRef.current) {
+            return resolve({
+              success: false,
+              message: 'Scroll ref does not exist. Will not scroll to view.',
+            });
+          }
+          if (!targetRefs.current?.[name]) {
+            resolve({
+              success: false,
+              message:
+                'Anchor ref ' +
+                name +
+                ' does not exist. It will not scroll. Please make sure to use the ScrollView provided by @nandorojo/anchors, or use the registerScrollRef function for your own ScrollView.',
+            });
+          }
+          targetRefs.current?.[name].measureLayout(
+            node,
+            (left, top) => {
+              requestAnimationFrame(() => {
+                const scrollY = top;
+                const scrollX = left;
+                const scrollable = getScrollableNode(
+                  scrollRef
+                ) as ScrollableWrapper;
+
+                let scrollTo = horizontal ? scrollX : scrollY;
+                scrollTo += offset;
+                scrollTo = Math.max(scrollTo, 0);
+
+                const key = horizontal ? 'x' : 'y';
+
+                if ('scrollTo' in scrollable) {
+                  scrollable.scrollTo({
+                    [key]: scrollTo,
+                    animated,
+                  });
+                } else if ('scrollToOffset' in scrollable) {
+                  scrollable.scrollToOffset({
+                    offset: scrollTo,
+                    animated,
+                  });
+                } else if ('scrollResponderScrollTo' in scrollable) {
+                  scrollable.scrollResponderScrollTo({
+                    [key]: scrollTo,
+                    animated,
+                  });
+                }
+                resolve({ success: true });
+              });
+            },
+            () => {
+              resolve({
+                success: false,
+                message: 'Failed to measure target node.',
+              });
+            }
+          );
+        });
+      },
+    }),
+    [horizontal, scrollRef, targetRefs]
+  );
+}
+
+function useRegisterScroller() {
+  const { registerScrollRef } = useAnchorsContext();
+
+  return { registerScrollRef };
+}
+
+function AnchorProvider({
+  children,
+  horizontal,
+}: { children: ReactNode } & Pick<AnchorsContext, 'horizontal'>) {
+  const value = useCreateAnchorsContext({ horizontal });
+
+  return (
+    <AnchorsContext.Provider value={value}>{children}</AnchorsContext.Provider>
+  );
+}
+
+/**
+ * Identical to the normal React Native `ScrollView`, except that it allows scrolling to anchor links.
+ *
+ * If you use this component, you don't need to use the `AnchorProvider`. It implements it for you.
+ */
+const ScrollView = forwardRef<
+  NativeScrollView,
+  ComponentProps<typeof NativeScrollView> & {
+    children?: ReactNode;
+  }
+>(function ScrollView({ horizontal = false, ...props }, ref) {
+  return (
+    <AnchorProvider horizontal={horizontal}>
+      <AnchorsContext.Consumer>
+        {({ registerScrollRef }) => (
+          <NativeScrollView
+            horizontal={horizontal}
+            {...props}
+            ref={mergeRefs([registerScrollRef, ref])}
+          />
+        )}
+      </AnchorsContext.Consumer>
+    </AnchorProvider>
+  );
+});
+
+/**
+ * Identical to the normal React Native flatlist, except that it allows scrolling to anchor links.
+ *
+ * If you use this component, you don't need to use the `AnchorProvider`.
+ *
+ * One important difference: if you want to use the `ref`, pass it to `flatListRef` instead of `ref`.
+ */
+function FlatList<T = any>({
+  flatListRef,
+  horizontal = false,
+  ...props
+}: FlatListProps<T> & { flatListRef?: RefObject<NativeFlatList> }) {
+  return (
+    <AnchorProvider horizontal={horizontal}>
+      <AnchorsContext.Consumer>
+        {({ registerScrollRef }) => (
+          <NativeFlatList
+            {...props}
+            ref={mergeRefs([registerScrollRef, flatListRef || null])}
+          />
+        )}
+      </AnchorsContext.Consumer>
+    </AnchorProvider>
+  );
+}
+
+function ScrollTo({
+  target,
+  onPress,
+  options,
+  onRequestScrollTo,
+  ...props
+}: {
+  children?: ReactNode;
+  target: Anchor;
+  options?: ScrollToOptions;
+  onRequestScrollTo?: (
+    props: Unpromisify<ReturnType<ReturnType<typeof useScrollTo>['scrollTo']>>
+  ) => void;
+} & ComponentProps<typeof Pressable>) {
+  const { scrollTo } = useScrollTo();
+
+  return (
+    <Pressable
+      {...props}
+      onPress={async (e) => {
+        onPress?.(e);
+        const result = await scrollTo(target, options);
+        onRequestScrollTo?.(result);
+      }}
+    />
+  );
+}
+
+const Target = forwardRef<
+  View,
+  { name: Anchor; children?: ReactNode } & ComponentProps<typeof View>
+>(function Target({ name, ...props }, ref) {
+  const { register } = useRegisterTarget();
+
+  return <View {...props} ref={mergeRefs([register(name), ref])} />;
+});
+
+export {
+  AnchorProvider,
+  ScrollView,
+  FlatList,
+  useRegisterTarget,
+  useScrollTo,
+  ScrollTo,
+  Target,
+  ScrollTo as Anchor,
+  useRegisterScroller,
+};
+// }
